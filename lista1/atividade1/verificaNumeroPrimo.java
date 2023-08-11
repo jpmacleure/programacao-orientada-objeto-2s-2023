f659fd1bd454f2c312e51bc837f7d1e156765d69
jpmacleure <macleure-@hotmail.com>
2023-08-11 10:38:49 -0300

    adiciona atividade 1

---
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,17 @@
+package lista1.atividade1;
+
+/**
+ * verificaNumeroPrimo
+ */
+public class verificaNumeroPrimo {
+
+    public static void main(String args[]) {
+        System.out.println("Ola, mundo!");
+        verificarNumeroPrimo(1232132131);
+    }
+
+    public static void verificarNumeroPrimo(Integer n){
+
+    }
+    
+}
